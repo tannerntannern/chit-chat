92be5bf8f0cc958d14cd697e3e4c5fac83851e11
Tanner Nielsen <tannerntannern@gmail.com>
2018-10-11 12:03:47 -0500

    feat: Got a start implementing the SocketClient

--- src/client/socket-client.ts
+++ src/client/socket-client.ts
@@ -1,3 +1,20 @@
-export class SocketClient {
+import * as socketio from 'socket.io-client';
+import {TransmitterStructure} from '../interface/socket-interface';
 
+export class SocketClient<API extends TransmitterStructure> {
+	/**
+	 * TODO: ...
+	 */
+	protected io: SocketIOClient.Socket;
+
+	/**
+	 * TODO: ...
+	 */
+	public connect(url: string = '', options?: SocketIOClient.ConnectOpts): Promise<boolean> {
+		return new Promise<boolean>((resolve, reject) => {
+			this.io = socketio.connect(url, options);
+			this.io.once('connect', () => { resolve(true) });
+			this.io.once('connect_failed', () => { resolve(false) });
+		});
+	}
 }
