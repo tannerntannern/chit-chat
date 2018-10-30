6ae6d6466ecc42ac312942907144c22adf8a4f55
Tanner Nielsen <tannerntannern@gmail.com>
2018-10-30 09:24:26 -0500

    test: Implemented some tests for the ExpressServer.

--- test/unit/ExpressServer.test.ts
+++ test/unit/ExpressServer.test.ts
@@ -1,5 +1,7 @@
 import 'mocha';
 import {expect} from 'chai';
+import * as path from 'path';
+import axios from 'axios';
 import {ExpressServer} from '../../src';
 import {HttpHandlers} from '../../src/interface/http-interface';
 import {HandlerCtx} from '../../src/server/express-server';
@@ -41,5 +43,55 @@ describe('ExpressServer', function(){
 		s = new Server();
 	});
 
-	// TODO: ...
+	describe('Configuration', function(){
+		it('should have the proper default configurations', async function(){
+			expect(s.config.serveStaticDir).to.equal(null);
+
+			await s.start();
+			expect((await axios.get('http://localhost:3000/')).data).to.equal(
+				'<h1>It Works!</h1>' +
+				'<p>The next step is to configure the server for your needs.</p>'
+			);
+
+			await s.stop();
+		});
+
+		it('should properly serve a static directory', async function(){
+			s.configure({
+				expressConfig: () => null,
+				serveStaticDir: path.resolve('./test/unit/serve')
+			});
+
+			await s.start();
+			expect((await axios.get('http://localhost:3000/test.html')).data).to.equal(
+				'<h1>Hello there</h1>'
+			);
+
+			await s.stop();
+		});
+	});
+
+	describe('Using an API', function(){
+		beforeEach(async () => {
+			await s.start();
+		});
+
+		afterEach(async () => {
+			await s.stop();
+		});
+
+		it('should pass a basic ping-pong test', async function(){
+			expect((await axios.get('http://localhost:3000/ping')).data).to.equal('pong');
+		});
+
+		it('should be able to use PUT /data and GET /data to modify and view data on the server', async function(){
+			expect(s.internalData).to.equal('default');
+			expect((await axios.get('http://localhost:3000/data')).data).to.equal('default');
+
+			await axios.put('http://localhost:3000/data', {value: 'test-data'});
+
+			expect(s.internalData).to.equal('test-data');
+			expect((await axios.get('http://localhost:3000/data')).data).to.equal('test-data');
+		});
+	});
 });
