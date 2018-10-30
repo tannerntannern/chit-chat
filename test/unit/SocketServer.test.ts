3fa1925591f1a343abb5f269be2d0371ecf3530e
Tanner Nielsen <tannerntannern@gmail.com>
2018-10-30 12:17:47 -0500

    test: Started the test stub for SocketServer.

--- test/unit/SocketServer.test.ts
+++ test/unit/SocketServer.test.ts
@@ -1,6 +1,22 @@
 import 'mocha';
 import {expect} from 'chai';
 import {SocketServer} from '../../src';
+import {SocketHandlers} from '../../src/interface/socket-interface';
+import {HandlerCtx} from '../../src/server/socket-server';
+
+interface API {
+	server: {
+
+	},
+	client: {
+
+	}
+}
+
+class Server extends SocketServer<API> {
+	protected socketHandlers: SocketHandlers<API, "server", HandlerCtx<API>>;
+
+}
 
 describe('SocketServer', function(){
 	// TODO: ...
