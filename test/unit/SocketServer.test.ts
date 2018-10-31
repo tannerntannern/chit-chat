c396a3716e54e30b7853a24ffc8d14eae793f43e
Tanner Nielsen <tannerntannern@gmail.com>
2018-10-30 21:07:25 -0500

    test: Some test stubs.

--- test/unit/SocketServer.test.ts
+++ test/unit/SocketServer.test.ts
@@ -6,16 +6,32 @@ import {HandlerCtx} from '../../src/server/socket-server';
 
 interface API {
 	server: {
-
+		'patch-data': {args: [string, string]}
 	},
 	client: {
-
+		'get-data': {args: [string], expect: 'patch-data'},
+		'put-data': {args: [string, string], expect: 'patch-data'}
 	}
 }
 
 class Server extends SocketServer<API> {
-	protected socketHandlers: SocketHandlers<API, "server", HandlerCtx<API>>;
+	protected data = {};
 
+	protected socketHandlers: SocketHandlers<API, "server", HandlerCtx<API>> = {
+		'get-data': (key: string) => {
+			return {
+				name: 'patch-data',
+				args: [key, this.data[key]]
+			}
+		},
+		'put-data': (key: string, value: string) => {
+			this.data[key] = value;
+			return {
+				name: 'patch-data',
+				args: [key, value]
+			}
+		}
+	};
 }
 
 describe('SocketServer', function(){
