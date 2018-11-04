90c64979777a245e13311d302ede47faa0209d25
Tanner Nielsen <tannerntannern@gmail.com>
2018-11-03 21:43:44 -0500

    feat: implemented the SocketExpressClient

--- src/client/socket-express-client.ts
+++ src/client/socket-express-client.ts
@@ -1,10 +1,18 @@
-import {Mixin, MixinDecorator} from 'ts-mixer';
+import {MixinDecorator} from 'ts-mixer';
 import {SocketClient} from './socket-client';
 import {ExpressClient} from './express-client';
+import {SocketInterface} from '../interface/socket-interface';
+import {HttpInterface} from '../interface/http-interface';
 
 /**
- * TODO: ...
+ * Simply mixes SocketClient and ExpressClient together into one class.
  */
-export class SocketExpressClient {
+// @ts-ignore: We know that SocketClient is abstract and we want it to stay that way
+@MixinDecorator(SocketClient, ExpressClient)
+abstract class SocketExpressClient<SocketAPI extends SocketInterface, HttpAPI extends HttpInterface> {}
 
+interface SocketExpressClient<SocketAPI extends SocketInterface, HttpAPI extends HttpInterface> extends SocketClient<SocketAPI>, ExpressClient<HttpAPI> {}
+
+export {
+	SocketExpressClient
 }
