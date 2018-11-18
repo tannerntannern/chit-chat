16563d4696beadd8ae357d475b51175731fbb322
Tanner Nielsen <tannerntannern@gmail.com>
2018-11-18 08:40:37 -0600

    feat: exposed the server configs

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import {HttpServer} from './server/http-server';
-import {ExpressServer} from './server/express-server';
+import {HttpServer, HttpServerConfig} from './server/http-server';
+import {ExpressServer, ExpressServerConfig} from './server/express-server';
 import {ExpressClient} from './client/express-client';
-import {SocketServer} from './server/socket-server';
+import {SocketServer, SocketServerConfig} from './server/socket-server';
 import {SocketClient} from './client/socket-client';
-import {SocketExpressServer} from './server/socket-express-server';
+import {SocketExpressServer, SocketExpressServerConfig} from './server/socket-express-server';
 import {SocketExpressClient} from './client/socket-express-client';
 
 export {
-	HttpServer,
-	ExpressServer,
+	HttpServer, HttpServerConfig,
+	ExpressServer, ExpressServerConfig,
 	ExpressClient,
-	SocketServer,
+	SocketServer, SocketServerConfig,
 	SocketClient,
-	SocketExpressServer,
+	SocketExpressServer, SocketExpressServerConfig,
 	SocketExpressClient
 };
