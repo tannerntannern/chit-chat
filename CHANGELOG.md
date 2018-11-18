8e56b3d87bc49b6e3dbb89ee51610f07ae42911e
Tanner Nielsen <tannerntannern@gmail.com>
2018-11-18 08:42:10 -0600

    chore(release): 0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,16 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+<a name="0.1.0"></a>
+# [0.1.0](https://github.com/tannerntannern/table-talk/compare/v0.1.0-beta.2...v0.1.0) (2018-11-18)
+
+
+### Features
+
+* exposed the server configs ([16563d4](https://github.com/tannerntannern/table-talk/commit/16563d4))
+
+
+
 <a name="0.1.0-beta.2"></a>
 # [0.1.0-beta.2](https://github.com/tannerntannern/table-talk/compare/v0.1.0-beta.1...v0.1.0-beta.2) (2018-11-17)
 
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,8 @@
-import { HttpServer } from './server/http-server';
-import { ExpressServer } from './server/express-server';
+import { HttpServer, HttpServerConfig } from './server/http-server';
+import { ExpressServer, ExpressServerConfig } from './server/express-server';
 import { ExpressClient } from './client/express-client';
-import { SocketServer } from './server/socket-server';
+import { SocketServer, SocketServerConfig } from './server/socket-server';
 import { SocketClient } from './client/socket-client';
-import { SocketExpressServer } from './server/socket-express-server';
+import { SocketExpressServer, SocketExpressServerConfig } from './server/socket-express-server';
 import { SocketExpressClient } from './client/socket-express-client';
-export { HttpServer, ExpressServer, ExpressClient, SocketServer, SocketClient, SocketExpressServer, SocketExpressClient };
+export { HttpServer, HttpServerConfig, ExpressServer, ExpressServerConfig, ExpressClient, SocketServer, SocketServerConfig, SocketClient, SocketExpressServer, SocketExpressServerConfig, SocketExpressClient };
