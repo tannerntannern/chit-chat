837a8ac6dbf5852ebda70ddb06c5eebe3e9a03b0
Tanner Nielsen <tannerntannern@gmail.com>
2018-11-18 16:56:30 -0600

    feat: exposed more types from the servers and interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import {HttpServer, HttpServerConfig} from './server/http-server';
-import {ExpressServer, ExpressServerConfig} from './server/express-server';
+import {ExpressServer, ExpressServerConfig, HandlerCtx as ExpressHandlerCtx} from './server/express-server';
 import {ExpressClient} from './client/express-client';
-import {SocketServer, SocketServerConfig} from './server/socket-server';
+import {SocketServer, SocketServerConfig, HandlerCtx as SocketHandlerCtx} from './server/socket-server';
 import {SocketClient} from './client/socket-client';
 import {SocketExpressServer, SocketExpressServerConfig} from './server/socket-express-server';
 import {SocketExpressClient} from './client/socket-express-client';
+import {HttpHandlers} from './interface/http-interface';
+import {SocketHandlers} from './interface/socket-interface';
 
 export {
+	HttpHandlers, SocketHandlers,
 	HttpServer, HttpServerConfig,
-	ExpressServer, ExpressServerConfig,
+	ExpressServer, ExpressServerConfig, ExpressHandlerCtx,
 	ExpressClient,
-	SocketServer, SocketServerConfig,
+	SocketServer, SocketServerConfig, SocketHandlerCtx,
 	SocketClient,
 	SocketExpressServer, SocketExpressServerConfig,
 	SocketExpressClient
