feeb6323f275f8b2ac6a96787346cee19d33bb13
Tanner Nielsen <tannerntannern@gmail.com>
2018-11-26 14:20:14 -0600

    chore(release): 2.0.0-alpha.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,16 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+<a name="2.0.0-alpha.3"></a>
+# [2.0.0-alpha.3](https://github.com/tannerntannern/table-talk/compare/v2.0.0-alpha.2...v2.0.0-alpha.3) (2018-11-26)
+
+
+### Bug Fixes
+
+* corrected broken types in the express-client ([d4e756d](https://github.com/tannerntannern/table-talk/commit/d4e756d))
+
+
+
 <a name="2.0.0-alpha.2"></a>
 # [2.0.0-alpha.2](https://github.com/tannerntannern/table-talk/compare/v2.0.0-alpha.1...v2.0.0-alpha.2) (2018-11-26)
 
--- dist/client/express-client.d.ts
+++ dist/client/express-client.d.ts
@@ -1,5 +1,5 @@
 import { HttpInterface, Methods } from '../interface/http-interface';
-import _axios, { AxiosRequestConfig } from 'axios';
+import _axios, { AxiosRequestConfig, AxiosPromise } from 'axios';
 /**
  * Basic AJAX class than can be used in Node or the browser.  Essentially just wraps around some axios methods and
  * provides the proper typing based on the given HttpInterface, so that no requests are malformed.
@@ -24,27 +24,27 @@ export declare class ExpressClient<API extends HttpInterface> {
     request<M extends Methods, EP extends keyof API[M]>(endpoint: EP, config: AxiosRequestConfig & {
         method: M;
         data?: API[M][EP]['args'];
-    }): Promise<API[M][EP]['return']>;
+    }): AxiosPromise<API[M][EP]['return']>;
     /**
      * Sends a GET request to the given endpoint.  Note that since GET requests cannot have a body, the args are passed
      * as query parameters.
      */
-    get<EP extends keyof API['get']>(endpoint: EP, args: API['get'][EP]['args'], config?: AxiosRequestConfig): Promise<API['get'][EP]['return']>;
+    get<EP extends keyof API['get']>(endpoint: EP, args: API['get'][EP]['args'], config?: AxiosRequestConfig): AxiosPromise<API['get'][EP]['return']>;
     /**
      * Sends a DELETE request to the given endpoint.  Note that since DELETE requests cannot have a body, the args are
      * passed as query parameters.
      */
-    delete<EP extends keyof API['delete']>(endpoint: EP, args: API['delete'][EP]['args'], config?: AxiosRequestConfig): Promise<API['delete'][EP]['return']>;
+    delete<EP extends keyof API['delete']>(endpoint: EP, args: API['delete'][EP]['args'], config?: AxiosRequestConfig): AxiosPromise<API['delete'][EP]['return']>;
     /**
      * Sends a POST request to the given endpoint with the given arguments.
      */
-    post<EP extends keyof API['post']>(endpoint: EP, args: API['post'][EP]['args'], config?: AxiosRequestConfig): Promise<API['post'][EP]['return']>;
+    post<EP extends keyof API['post']>(endpoint: EP, args: API['post'][EP]['args'], config?: AxiosRequestConfig): AxiosPromise<API['post'][EP]['return']>;
     /**
      * Sends a PUT request to the given endpoint with the given arguments.
      */
-    put<EP extends keyof API['put']>(endpoint: EP, args: API['put'][EP]['args'], config?: AxiosRequestConfig): Promise<API['put'][EP]['return']>;
+    put<EP extends keyof API['put']>(endpoint: EP, args: API['put'][EP]['args'], config?: AxiosRequestConfig): AxiosPromise<API['put'][EP]['return']>;
     /**
      * Sends a PATCH request to the given endpoint with the given arguments.
      */
-    patch<EP extends keyof API['patch']>(endpoint: EP, args: API['patch'][EP]['args'], config?: AxiosRequestConfig): Promise<API['patch'][EP]['return']>;
+    patch<EP extends keyof API['patch']>(endpoint: EP, args: API['patch'][EP]['args'], config?: AxiosRequestConfig): AxiosPromise<API['patch'][EP]['return']>;
 }
