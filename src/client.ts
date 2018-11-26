635648be3a65e98fe05996b3d1a8ab15f970eaa3
Tanner Nielsen <tannerntannern@gmail.com>
2018-11-26 13:26:29 -0600

    feat: added separate client and server exports to hopefully make tree-shaking work properly

---
+++ src/client.ts
@@ -0,0 +1,7 @@
+import {ExpressClient} from './client/express-client';
+import {SocketClient} from './client/socket-client';
+
+export {
+	ExpressClient,
+	SocketClient
+};
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
 import {HttpServer, HttpServerConfig} from './server/http-server';
 import {ExpressServerManager, ExpressServerManagerConfig} from './server/express-server';
-import {ExpressClient} from './client/express-client';
 import {SocketServerManager, SocketServerManagerConfig} from './server/socket-server';
-import {SocketClient} from './client/socket-client';
+import {SocketExpressClient} from './client/socket-express-client';
 import {HttpHandlers} from './interface/http-interface';
 import {SocketHandlers} from './interface/socket-interface';
 
@@ -10,7 +9,6 @@ export {
 	HttpHandlers, SocketHandlers,
 	HttpServer, HttpServerConfig,
 	ExpressServerManager, ExpressServerManagerConfig,
-	ExpressClient,
 	SocketServerManager, SocketServerManagerConfig,
-	SocketClient,
+	SocketExpressClient
 };
