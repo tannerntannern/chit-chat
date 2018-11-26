d4e756d79d4f628da3b8ec9b8c2706dd1058d24e
Tanner Nielsen <tannerntannern@gmail.com>
2018-11-26 14:19:10 -0600

    fix: corrected broken types in the express-client

--- src/client/express-client.ts
+++ src/client/express-client.ts
@@ -1,5 +1,5 @@
 import {HttpInterface, Methods} from '../interface/http-interface';
-import _axios, {AxiosRequestConfig} from 'axios';
+import _axios, {AxiosRequestConfig, AxiosPromise} from 'axios';
 
 declare let window: any;
 
@@ -39,7 +39,7 @@ export class ExpressClient<API extends HttpInterface> {
 	public request<M extends Methods, EP extends keyof API[M]>(
 		endpoint: EP,
 		config: AxiosRequestConfig & { method: M, data?: API[M][EP]['args'] }
-	): Promise<API[M][EP]['return']> {
+	): AxiosPromise<API[M][EP]['return']> {
 		Object.assign(config, {
 			url: this.host + endpoint
 		});
@@ -51,7 +51,7 @@ export class ExpressClient<API extends HttpInterface> {
 	 * Sends a GET request to the given endpoint.  Note that since GET requests cannot have a body, the args are passed
 	 * as query parameters.
 	 */
-	public get<EP extends keyof API['get']>(endpoint: EP, args: API['get'][EP]['args'], config?: AxiosRequestConfig): Promise<API['get'][EP]['return']> {
+	public get<EP extends keyof API['get']>(endpoint: EP, args: API['get'][EP]['args'], config?: AxiosRequestConfig): AxiosPromise<API['get'][EP]['return']> {
 		if (config === undefined) config = {};
 		config.params = args;
 
@@ -62,7 +62,7 @@ export class ExpressClient<API extends HttpInterface> {
 	 * Sends a DELETE request to the given endpoint.  Note that since DELETE requests cannot have a body, the args are
 	 * passed as query parameters.
 	 */
-	public delete<EP extends keyof API['delete']>(endpoint: EP, args: API['delete'][EP]['args'], config?: AxiosRequestConfig): Promise<API['delete'][EP]['return']> {
+	public delete<EP extends keyof API['delete']>(endpoint: EP, args: API['delete'][EP]['args'], config?: AxiosRequestConfig): AxiosPromise<API['delete'][EP]['return']> {
 		if (config === undefined) config = {};
 		config.params = args;
 
@@ -72,21 +72,21 @@ export class ExpressClient<API extends HttpInterface> {
 	/**
 	 * Sends a POST request to the given endpoint with the given arguments.
 	 */
-	public post<EP extends keyof API['post']>(endpoint: EP, args: API['post'][EP]['args'], config?: AxiosRequestConfig): Promise<API['post'][EP]['return']> {
+	public post<EP extends keyof API['post']>(endpoint: EP, args: API['post'][EP]['args'], config?: AxiosRequestConfig): AxiosPromise<API['post'][EP]['return']> {
 		return ExpressClient.axios.post(this.host + endpoint, args, config);
 	}
 
 	/**
 	 * Sends a PUT request to the given endpoint with the given arguments.
 	 */
-	public put<EP extends keyof API['put']>(endpoint: EP, args: API['put'][EP]['args'], config?: AxiosRequestConfig): Promise<API['put'][EP]['return']> {
+	public put<EP extends keyof API['put']>(endpoint: EP, args: API['put'][EP]['args'], config?: AxiosRequestConfig): AxiosPromise<API['put'][EP]['return']> {
 		return ExpressClient.axios.put(this.host + endpoint, args, config);
 	}
 
 	/**
 	 * Sends a PATCH request to the given endpoint with the given arguments.
 	 */
-	public patch<EP extends keyof API['patch']>(endpoint: EP, args: API['patch'][EP]['args'], config?: AxiosRequestConfig): Promise<API['patch'][EP]['return']> {
+	public patch<EP extends keyof API['patch']>(endpoint: EP, args: API['patch'][EP]['args'], config?: AxiosRequestConfig): AxiosPromise<API['patch'][EP]['return']> {
 		return ExpressClient.axios.patch(this.host + endpoint, args, config);
 	}
 }
